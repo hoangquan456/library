9784a096e4c01eabaff5fdca442309c1d178b43e
Quan Truong <truonghoangquan456@gmail.com>
2025-03-18 20:05:32 -0400

    implement form handling to add new manga

--- script.js
+++ script.js
@@ -33,10 +33,44 @@ class UI { //control DOM and screen render
 
     constructor(library) {
         this.library = library;
-        let addbtn = document.querySelector(".add-manga"); 
-        addbtn.addEventListener("click", ()=>{
-            this.library.addManga("abc", "def");
+        this.showDialog();
+        
+    }
+
+    showDialog() {
+        const addBtn = document.querySelector(".add-manga");
+        const dialog = document.querySelector("#mangaDialog");
+        const form = document.querySelector("#mangaForm");
+        const closeBtn = document.querySelector("#closeBtn");
+
+        // Show dialog when "Add Manga" button is clicked
+        addBtn.addEventListener("click", () => {
+            dialog.showModal();
+        });
+
+        // Close dialog without submitting
+        closeBtn.addEventListener("click", () => {
+            dialog.close();
+        });
+
+        // Handle form submission
+        form.addEventListener("submit", (event) => {
+            event.preventDefault(); // Prevent default form submission
+
+            // Get form data
+            const formData = new FormData(form);
+            const title = formData.get("title");
+            const author = formData.get("author");
+            const chapters = parseInt(formData.get("chapters"));
+            const coverImage = formData.get("coverImage") || ""; // Default to empty string if not provided
+            const description = formData.get("description") || ""; // Default to empty string
+            const read = formData.get("read") === "true"; // Convert string to boolean
+
+            // Add manga to library and update UI
+            this.library.addManga(title, author, chapters, coverImage, description, read);
             this.render();
+            dialog.close(); // Close dialog after adding
+            form.reset(); // Clear form for next use
         });
     }
     
@@ -136,140 +170,3 @@ ui.library.addManga("Gintama", "Hideaki Sorachi", "702", "https://comicvine.game
 ui.library.addManga("Attack on Titan", "Hajime Isayama", "139", "https://upload.wikimedia.org/wikipedia/en/d/d6/Shingeki_no_Kyojin_manga_volume_1.jpg", "In 100 years, humanity lives in fear of the Titans. A race of beasts who take the appearance of humans but are much larger and lack sexual organs. Their only purpose is to feast upon humans. However, humanity barricaded themselves in three 50-meter walls to protect themselves from the terror of the Titans.", true);
 // console.log(ui.myLibrary);
 ui.render(); 
-
-
-// let myLibrary = []; 
-
-// function Book(title, author, genre, pages, read = false) {
-//     this.title = title;
-//     this.author = author;
-//     this.genre = genre;
-//     this.pages = pages;
-//     this.read = read;
-//     this.id = crypto.randomUUID();
-// }
-
-// Book.prototype.status = function() {
-//     if (this.read) return "read";
-//     else return "unread";
-// }
-
-
-// Book.prototype.changeStatus = function() {
-//     this.read = !this.read;
-// }
-
-// function addBookToLibrary(title, author, genre, pages, read) {
-//     let book = new Book(title, author, genre, pages, read); 
-//     myLibrary.push(book); 
-// }
-
-// addBookToLibrary("The Midnight Library", "Matt Haig", "Fiction, Fantasy", "300", true);
-// addBookToLibrary("To Kill a Mockingbird", "Harper Lee", "Fiction, Classic", "324");
-
-
-// function displayLibrary() {
-//     let bookshelf = document.getElementById("bookshelf"); 
-//     bookshelf.innerHTML = "";
-//     myLibrary.forEach((book) => {
-//         // Create the card container
-//         let card = document.createElement("div"); 
-//         card.setAttribute("class", "card");
-
-//         // Create and append the title element
-//         let title = document.createElement("div");
-//         title.setAttribute("class", "title");
-//         title.innerText = book.title;
-//         card.appendChild(title);
-
-//         // Create and append the author element
-//         let author = document.createElement("div");
-//         author.innerText = `Author: ${book.author}`;
-//         card.appendChild(author);
-
-//         // Create and append the genre element
-//         let genre = document.createElement("div");
-//         genre.innerText = `Genre: ${book.genre}`;
-//         card.appendChild(genre);
-
-//         // Create and append the pages element
-//         let pages = document.createElement("div");
-//         pages.innerText = `Pages: ${book.pages}`;
-//         card.appendChild(pages);
-
-//         let read = document.createElement("button"); 
-//         read.innerText = book.status();
-//         read.addEventListener("click", (e) => {
-//             book.changeStatus();
-//             read.innerText = book.status();
-//         });
-//         read.setAttribute("class", "remove");
-//         card.appendChild(read);
-
-//         // Create the remove button
-//         let remove = document.createElement("button"); 
-//         remove.innerText = "Remove";
-//         remove.setAttribute("data-id", book.id);
-//         remove.addEventListener("click", (e) => {
-//             let bookId = e.target.getAttribute("data-id"); 
-//             removeBook(bookId); 
-//         });
-//         remove.setAttribute("class", "remove");
-
-//         // Append the remove button to the card
-//         card.appendChild(remove);
-
-//         // Append the card to the bookshelf
-//         bookshelf.appendChild(card);
-//     });
-// }
-
-// function removeBook(id) {
-//     console.log(id);
-    
-//     myLibrary = myLibrary.filter( (book) => {
-//         return book.id != id;
-//     });
-//     displayLibrary();
-// }
-
-// displayLibrary();
-
-// const showButton = document.querySelector("dialog + button");
-// const closeButton = document.getElementById("closeBtn"); 
-// const dialog = document.querySelector("dialog");
-// showButton.addEventListener("click", (e) =>{
-//     dialog.showModal();
-// });
-// closeButton.addEventListener("click", (e)=>{
-//     dialog.close();
-// });
-
-// const submit = document.getElementById("submit"); 
-// const titleInput = document.getElementById("title"); // Title input
-// const authorInput = document.getElementById("author"); // Author input
-// const genreInput = document.getElementById("genre"); // Genre input
-// const pagesInput = document.getElementById("pages"); // Pages input
-// const myForm  = document.getElementById("myForm");
-// myForm.addEventListener("submit", (e) => {
-//     if (myForm.checkValidity()) {
-//         e.preventDefault();
-//         const title = titleInput.value;
-//         const author = authorInput.value;
-//         const genre = genreInput.value;
-//         const pages = pagesInput.value;
-//         const selectedOption = document.querySelector('input[name="choice"]:checked');
-//         console.log(selectedOption.getAttribute("id"));
-        
-//         addBookToLibrary(title, author, genre, pages, selectedOption.getAttribute("id") == "yes");
-//         displayLibrary();
-        
-//         titleInput.value = "";
-//         authorInput.value = "";
-//         genreInput.value = "";
-//         pagesInput.value = "";
-    
-//         dialog.close();
-//     }
-// });
-
